982312345f11807c5ceab1e9261c614bc17fb152
GatiMishra <mishrakgati@gmail.com>
2018-03-14 18:48:05 +0530

    commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from sqlalchemy import Column, Integer, Sequence, String, ForeignKey, DateTime
+from datetime import datetime
+import psycopg2
 
 app = Flask(__name__)
 

0712e05b0138fe20acf3a9da8d2504099b038958
GatiMishra <mishrakgati@gmail.com>
2018-03-14 18:02:28 +0530

    commit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from flask import Flask,render_template
-from sqlalchemy import create_engine,Column, String, Integer, Date,asc, Sequence
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import *
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 

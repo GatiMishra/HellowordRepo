27b0fa415aef63683fbe8549a41715beeb0c0c26
GatiMishra <mishrakgati@gmail.com>
2018-03-15 15:52:02 +0530

    commit

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ def featureRequestDetails():
 def featureRequestForm():
     """ featureRequestForm function is used to display featureRequestForm html page"""
     return render_template('featureRequestForm.html')
-
-app.config.from_object(Config)
 	
 if __name__ == '__main__':
   app.run()
